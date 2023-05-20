7e2015d06ae199abb110f4f3dfe09f27f811041a
Sofia <sofia.oyola@upb.edu.co>
2023-05-19 20:59:54 -0500

    rig sofia

--- Demo/Assets/Scripts/AnimationController.cs
+++ Demo/Assets/Scripts/AnimationController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class AnimationController : MonoBehaviour
 {
+    [SerializeField] Animator animator;
     public void SetMotionValue(float value)
     {
         GetComponent<Animator>().SetFloat("MovementSpeed", value);
@@ -23,6 +24,30 @@ public class AnimationController : MonoBehaviour
     {
         
     }
-    
-    
+
+    private void Update()
+    {
+        if(Input.GetKey("w"))
+        {
+            animator.SetBool("IsWalking",true);
+            animator.SetBool("Isidle", false);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+            animator.SetBool("Isidle", true);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("IsWalking", true);
+            animator.SetBool("Isidle", false);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+            animator.SetBool("Isidle", true);
+        }
+    }
+
 }
